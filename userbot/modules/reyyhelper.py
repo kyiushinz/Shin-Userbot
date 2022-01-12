7db59325f1177dfb07a96b4342c2f572e1c598e9
ReyyNada <94505216+ReyyNada@users.noreply.github.com>
2022-01-12 15:12:11 +0700

    Update and rename bdrlhelper.py to reyyhelper.py

--- userbot/modules/reyyhelper.py
+++ userbot/modules/reyyhelper.py
@@ -12,16 +12,16 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/Bdrllll)"
-        "\n[Repo](https://github.com/Yansaii/Bdrl-userbot)"
-        "\n[Instagram](instagram.com/kechili.8)")
+        "\n[Telegram](t.me/SangDappaa)"
+        "\n[Repo](https://github.com/ReyyNada/Reyy-userbot)"
+        "\n[Instagram](instagram.com/woi_dan1)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Yansaii/Bdrl-userbot/Bdrl-userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ReyyNada/Reyy-userbot/Reyy-userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
