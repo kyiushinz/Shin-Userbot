c8a0f039fe48aa33d4ca49cf7a269e9fcfac61b9
ReyyNada <94505216+ReyyNada@users.noreply.github.com>
2022-01-12 15:22:06 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,9 +33,9 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t ✗  "
-        await event.edit("**🔥ʙᴅʀʟ-ᴜsᴇʀʙᴏᴛ🔥**\n\n"
+        await event.edit("**🔥ʀᴇʏʏ-ᴜsᴇʀʙᴏᴛ🔥**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-                         f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n☞  Onwer : @Bdrllll")
+                         f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n☞  Onwer : @SangDappaa")
         await asyncio.sleep(1000)
         await event.delete()
